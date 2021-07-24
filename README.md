47691d4a01f188d59c7443c9b45201be20f7c714
Huakang <huakang.li@outlook.com>
2021-07-24 20:54:16 +0800

    add README.md generator

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ python contents.py
 ```
 
 ```md
-笔记索引目录结构
+笔记目录索引结构
 # our-CS-notes
 ## second-level directory name
 ### third-level directory name
@@ -15,29 +15,31 @@ python contents.py
 [markdown file title name](/path/name)
 ```
 
-## MySQL
 
-[1.登陆退出相关](./MySQL/1%20启动退出.md)
+##JavaScript
+[API.md](/JavaScript/API.md)
+[jquery.md](/JavaScript/jquery.md)
+[js_event_and_DOM.md](/JavaScript/js_event_and_DOM.md)
 
-[2.语法规范](./MySQL/2%20语法规范.md)
+##Java_Basic
+###IO流_反射
+[IO流.md](/Java_Basic/IO流_反射/IO流.md)
+###多线程
+[note.md](/Java_Basic/多线程/note.md)
+###常用类_集合_泛型
+[note_常用类.md](/Java_Basic/常用类_集合_泛型/note_常用类.md)
 
-[3.客户端下载之后](./MySQL/3%20客户端下载.md)
+##Linux
+[Linux_Basic.md](/Linux/Linux_Basic.md)
 
-[4.DQL语言基础](./MySQL/4%20DQL语言基础.md)
-
-[5.DML语言基础](./MySQL/5%20DML语言基础.md)
-
-[6.DDL语言基础](./MySQL/6%20DDL语言基础.md)
-
-[7.TCL语言](./MySQL/7%20TCL语言.md)
-
-[8.视图讲解](./MySQL/8%20视图讲解.md)
-
-[9.存储过程和函数](./MySQL/9%20存储过程和函数.md)
-
-[10.流程控制结构](./MySQL/10%20流程控制结构.md)
-
-## JavaScript
-
-[事件和DOM](./JavaScript/js_event_and_DOM.md)
-
+##MySQL
+[01 启动退出.md](/MySQL/01%20启动退出.md)
+[02 语法规范.md](/MySQL/02%20语法规范.md)
+[03 客户端下载.md](/MySQL/03%20客户端下载.md)
+[04 DQL语言基础.md](/MySQL/04%20DQL语言基础.md)
+[05 DML语言基础.md](/MySQL/05%20DML语言基础.md)
+[06 DDL语言基础.md](/MySQL/06%20DDL语言基础.md)
+[07 TCL语言.md](/MySQL/07%20TCL语言.md)
+[08 视图讲解.md](/MySQL/08%20视图讲解.md)
+[09 存储过程和函数.md](/MySQL/09%20存储过程和函数.md)
+[10 流程控制结构.md](/MySQL/10%20流程控制结构.md)
--- contents.py
+++ contents.py
@@ -5,3 +5,80 @@ add index of notes to README.md
 
 """
 
+import os
+import re
+
+exclude_dirs = [".git", "figures"]
+exclude_files = ["README.md", "test.md"]
+
+front = """# our-CS-notes
+CS notes from [Sakana](https://github.com/NinomiyaSakana) & [Kayasu](https://github.com/Li-Huakang) :yellow_heart:
+自学笔记 不定期更新
+```bash
+# 更新目录索引README.md
+python contents.py
+```
+
+```md
+笔记目录索引结构
+# our-CS-notes
+## second-level directory name
+### third-level directory name
+...
+[markdown file title name](/path/name)
+```
+
+"""
+
+
+def walktree(top, rst):
+    flag = False
+    with os.scandir(top) as entries:
+        for entry in entries:
+            if  entry.is_dir():
+                if entry.name in exclude_dirs:
+                    continue
+                #dir
+                dir = entry.name
+                rst[dir]={}
+                path = os.path.join(top, entry.name)
+                hasmd = walktree(path, rst[dir])
+                flag = hasmd or flag
+                if not hasmd:
+                    rst.pop(dir)
+            else:
+                #file
+                if entry.name.endswith('.md'):
+                    rst[entry.name] = None
+                    flag = True or flag
+                else:
+                    flag = False or flag
+    return flag
+
+
+def gen(res, level, f, path=''):
+    for key in sorted(res):
+        val = res[key]
+        if(val==None):#.md
+            if key in exclude_files:
+                continue
+            str = '[' + key + ']' + '(' + path + '/' + key.replace(' ', '%20') + ')' + '\n'
+            f.write(str)
+        else:#dir
+            if level==2:
+                str = '\n'+'#'*level+key+'\n'
+            else:
+                str = '#'*level+key+'\n'
+            f.write(str)
+            gen(val, level+1, f, path+'/'+key)
+
+def main():
+    basepath = './'
+    res = {}
+    walktree(basepath, res)
+    with open('README.md', 'w+') as f:
+        f.write(front)
+        gen(res,2,f)
+
+if __name__ == "__main__":
+    main()
